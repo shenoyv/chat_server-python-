22ea3ffbbcce2262fedd573c99780b4a1394f988
shenoyv <shenoyv@tcd.ie>
2017-11-21 23:53:57 +0000

    defining main function

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import sys
 import select
+import socket
 
 HOST = '127.0.0.1'
 PORT = 8018
@@ -7,18 +8,18 @@ TIMEOUT = 5
 BUF_SIZE = 1024
 
 
-class WhatsUpClient():
+class Client():
 
     def __init__(self, host=HOST, port=PORT):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
-        #print(" connecting to %s:%s " %(host,port))
+        print(" connecting to %s:%s " %(host,port))
         while 1:
             try:
                 buf = self.sock.recv(BUF_SIZE)
                 sys.stdout.write(buf)
                 data = raw_input()
-                if cmd.strip() == "KILL_SERVICE":
+                if data.strip() == "KILL_SERVICE":
                     sys.exit(1)
                 self.sock.send(data)
             except:
@@ -26,10 +27,11 @@ class WhatsUpClient():
 
         def run(self):
             pass
+def main():
+    client = Client()
 
-
-
-
+if __name__ == '__main__':
+    main()
 
 
 
