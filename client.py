b89fb722ca6a4784650dafc22b1117aa7db7b1c3
shenoyv <shenoyv@tcd.ie>
2017-11-21 10:31:51 +0000

    Some Additions

--- client.py
+++ client.py
@@ -7,3 +7,17 @@ def chat_client():
     if (len(sys.argv) < 3):
         print 'Usage : python chat_client.py hostname port'
         sys.exit()
+
+    def chat_client():
+        if (len(sys.argv) < 3):
+            print 'Usage : python chat_client.py hostname port'
+            sys.exit()
+
+        host = sys.argv[1]
+        port = int(sys.argv[2])
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        while 1:
+            socket_list = [sys.stdin, s]
+
