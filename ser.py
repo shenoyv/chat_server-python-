97f0177d423f8bf41a7bb4df422857697b39869a
shenoyv <shenoyv@tcd.ie>
2017-11-23 21:00:54 +0000

    Deleted few lines and re-edited  the program

--- ser.py
+++ ser.py
@@ -1,77 +1,33 @@
 import sys
 import socket
 import select
+import threading
 
-HOST = ''
-SOCKET_LIST = []
-RECV_BUFFER = 4096
-PORT = 9009
-
-class Serv_chat(Threading.thread):
-    def __init__(self, conn, addr):
-        threading.Thread.__init__(self)
-        self.conn = conn
-        self.addr = addr
-        self.ip = self.addr[0]
-        self.name = ''
-
-    def Chatroom(self, room_name, msg):
-        global rooms
-        # if the group does not exist, create it
-        rooms.setdefault(room_name, set())
-
-        # if current user is a member of the group
-        if (self.conn, self.addr) in rooms[room_name]:
-            self.broadcast(msg, rooms[room_name])
-        else:
-
-               print("You are currently a member of group `%s`"% (room_name,))
-
-    def join(self,room_name):
-        global rooms
-        rooms[group_name].add((self.conn, self.addr))
-        print("## You have joined the group `%s`" %
-                             (group_name,))
-
-
-
-
-
-    def leave(self):
-
-
-
-
-
-    def broadcast(self):
-
-
-def main():
+class Server:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((HOST, PORT))
-    sock.listen(10)
-
-    # add server socket object to the list of readable connections
-    SOCKET_LIST.append(sock)
-    while True:
-        current_connection, address = sock.accept()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)#to reuse the same socket
+    connections=[]
+    def __init__(self):
+        self.sock.bind(('0.0.0.0'),8018)
+        self.sock.listen(10)
+    def handle_sock(self,conn,add_rs):
         while True:
-            data = current_connection.recv(2048)
+            data =conn.recv(1024)
+            print data
+            for conns in self.connections:
+                connections.send(data)
+            if not data:
+               break
+    
 
-            if data == 'KILL_SERVICE\r\n':
-                current_connection.shutdown(1)
-                current_connection.close()
-                break
 
-            elif data == 'stop\r\n':
-                current_connection.shutdown(1)
-                current_connection.close()
-                exit()
 
-            elif data:
-                current_connection.send(data)
-                print data
+
+
+
+
+
+
 
 
 
