6eb363d06a00af7325aac7eca5415b8bbe6a6d9e
shenoyv <shenoyv@tcd.ie>
2017-11-23 23:18:22 +0000

    few changes in both clients and servers

--- ser.py
+++ ser.py
@@ -4,12 +4,25 @@ import select
 import threading
 
 class Server:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)#to reuse the same socket
+
     connections=[]
+    clients =[]
     def __init__(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # to reuse the same socket
         self.sock.bind(('0.0.0.0'),8018)
         self.sock.listen(10)
+        while  True:
+           conn,add_rs = self. sock.accept()
+           Thread_s=threading.Thread(target=self.handle_sock, args=(conn,add_rs))
+           Thread_s.daemon = True
+           Thread_s.start()
+           self.connections.append(conn)
+           self.clients.append(add_rs[0])
+           print(self.connections)
+
+           #print("client: is connected)
+
     def handle_sock(self,conn,add_rs):
         while True:
             data =conn.recv(1024)
@@ -17,15 +30,24 @@ class Server:
             for conns in self.connections:
                 connections.send(data)
             if not data:
-               break
-    def  go(self):
-        while  True:
-           conn,add_rs = self. sock.accept()
-           Thread_s=threading.Thread(target=self.handle_sock, args=(c,a))
-           Thread_s.daemon = True
-           Thread_s.start()
-           self.connections.append(c)
-           print(self.connections)
+
+                #print("client_name",disconnected)
+                self.connections.remove(conn)
+                self.clients.remove(a[0])
+                conn.close()
+                self.send_clients()
+                break
+    def send_clients(self):
+        cx= ""
+        for cls in self.clients:
+            cx= cx + "cls" +","
+
+        for conns in self.connections:
+            conns.send(b'\x12'+bytes(cx, "utf-8"))
+
+
+
+
 
 server = Server()
 server.go()
